2ad58085dfcfcae760c2c140ed96e219775a02f0
zamigaliyev <zamiqazehernandez@gmail.com>
2021-05-04 00:57:20 +0400

    Todo project frontend and backend

--- code/todo/backend/todo_project/urls.py
+++ code/todo/backend/todo_project/urls.py
@@ -1,7 +1,7 @@
-"""library_project URL Configuration
+"""todo_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include  #new
+from django.urls import path, include # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('books.urls')),
-    path('api/',include('api.urls')), # new
+    path('api/', include('todos.urls')), # new
 ]
